1bf4d4f9684a5ae6c5504c7790fddec815fb0b3b
Martynas Sudintas <martynas.s@helis.lt>
2017-09-04 17:39:50 +0300

    improved array iterator aggregations

--- Result/ArrayIterator.php
+++ Result/ArrayIterator.php
@@ -10,6 +10,7 @@
  */
 
 namespace ONGR\ElasticsearchBundle\Result;
+use ONGR\ElasticsearchBundle\Result\Aggregation\AggregationValue;
 
 /**
  * Class DocumentIterator.
@@ -46,6 +47,39 @@ class ArrayIterator extends AbstractResultsIterator implements \ArrayAccess
     public function offsetUnset($offset)
     {
         unset($this->documents[$offset]);
+    }
+
+    /**
+     * Returns aggregations.
+     *
+     * @return array
+     */
+    public function getAggregations()
+    {
+        $aggregations = [];
+
+        foreach (parent::getAggregations() as $key => $aggregation) {
+            $aggregations[$key] = $this->getAggregation($key);
+        }
+
+        return $aggregations;
+    }
+
+    /**
+     * Get a specific aggregation by name. It fetches from the top level only.
+     *
+     * @param string $name
+     *
+     * @return AggregationValue|null
+     */
+    public function getAggregation($name)
+    {
+        $aggregations = parent::getAggregations();
+        if (!array_key_exists($name, $aggregations)) {
+            return null;
+        }
+
+        return new AggregationValue($aggregations[$name]);
     }
 
     /**
